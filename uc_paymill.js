b58a0e4e858e4648cbba458270dcd68b27b7a07d
Topsitemakers <support@topsitemakers.com>
2012-11-28 17:04:30 +0100

    Adding basic JS file

---
+++ uc_paymill.js
@@ -0,0 +1,17 @@
+
+/**
+ * @file
+ * JS for generating authentication tokens for Paymill.
+ *
+ * Created by: Topsitemakers
+ * http://www.topsitemakers.com/
+ */
+
+(function($) {
+  Drupal.behaviors.UCPaymill = {
+    
+    attach: function(context, settings) {
+      // 
+    }
+  };
+})(jQuery);
