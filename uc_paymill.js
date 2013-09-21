b450a131e1ba9a96695e88cceab278cf4ec21a9b
Topsitemakers <support@topsitemakers.com>
2013-09-21 21:21:01 +0200

    adding code to main JavaScript file which gets order information and retrieves the token from Paymill

--- uc_paymill.js
+++ uc_paymill.js
@@ -10,7 +10,37 @@
 (function($) {
   Drupal.behaviors.ucPaymill = {
     attach: function(context, settings) {
-      // 
+      window.PAYMILL_PUBLIC_KEY = Drupal.settings.ucPaymill.publicKey;
+
+      var submitForm = $('#uc-cart-checkout-form'),
+          submitButton = submitForm.find('#edit-continue'),
+          submitFormErrors = submitForm.find('#uc-paymill-errors');
+
+      submitForm.once('uc_paymill-processed').submit(function(e) {
+        // Build app Paymill parameters for this transaction.
+        var ucPaymillParams = {
+          number: submitForm.find('#edit-panes-payment-details-cc-number').val(),
+          exp_month: submitForm.find('#edit-panes-payment-details-cc-exp-month').val(),
+          exp_year: submitForm.find('#edit-panes-payment-details-cc-exp-year').val(),
+          cvc: submitForm.find('#edit-panes-payment-details-cc-cvv').val(),
+          amount: submitForm.find('#uc-paymill-order-total').val(),
+          currency: Drupal.settings.ucPaymill.currencyCode
+        };
+        // Get the Paymill token
+        paymill.createToken(ucPaymillParams, function(error, result) {
+          if (error) {
+            $('#uc-paymill-errors').val(error.apierror);
+          }
+          else {
+            $('#uc-paymill-token').val(result.token);
+          }
+          submitButton.click();
+        });
+        if ($('#uc-paymill-token').val() != '' || $('#uc-paymill-errors').val() != '') {
+          return true;
+        }
+        return false;
+      });
     }
   };
 })(jQuery);
